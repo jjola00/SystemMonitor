f2b654d7b7ec80d3193f2b1171d9b125dcc2eac4
Kami <jjolajitan2006@gmail.com>
2025-03-14 10:31:26 +0000

    pls

--- apis/main.py
+++ apis/main.py
@@ -1,9 +1,23 @@
+import os
+import sys
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from apis.api.system_router import router as system_router
-from apis.api.weather_router import router as weather_router
-from apis.api.crypto_router import router as crypto_router
-from apis.api.command_router import router as command_router
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir) if "apis" in current_dir else current_dir
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+try:
+    from api.system_router import router as system_router
+    from api.weather_router import router as weather_router
+    from api.crypto_router import router as crypto_router
+    from api.command_router import router as command_router
+except ImportError:
+    from apis.api.system_router import router as system_router
+    from apis.api.weather_router import router as weather_router
+    from apis.api.crypto_router import router as crypto_router
+    from apis.api.command_router import router as command_router
 
 app = FastAPI(title="System Monitor API")
 
