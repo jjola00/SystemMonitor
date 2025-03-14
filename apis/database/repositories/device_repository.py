ea6e67821f24fd4fcb797fcb37e59cfd144a4eeb
Kami <jjolajitan2006@gmail.com>
2025-03-14 10:23:07 +0000

    removed ip address

--- apis/database/repositories/device_repository.py
+++ apis/database/repositories/device_repository.py
@@ -5,14 +5,12 @@ class DeviceRepository:
     def __init__(self, db_client_instance=db_client):
         self.db_client = db_client_instance
 
-    def get_or_create_device_id(self, device_name, ip_address=None):
-        response = self.db_client.table("devices").select("id, ip_address").eq("name", device_name).execute()
+    def get_or_create_device_id(self, device_name):
+        response = self.db_client.table("devices").select("id").eq("name", device_name).execute()
         if response.data:
             device_id = response.data[0]["id"]
-            if ip_address and response.data[0].get("ip_address") != ip_address:
-                self.db_client.table("devices").update({"ip_address": ip_address}).eq("id", device_id).execute()
             return device_id
-        new_device = {"name": device_name, "ip_address": ip_address, "created_at": datetime.now(timezone.utc).isoformat()}
+        new_device = {"name": device_name, "created_at": datetime.now(timezone.utc).isoformat()}
         insert_response = self.db_client.table("devices").insert(new_device).execute()
         return insert_response.data[0]["id"] if insert_response.data else None
     
